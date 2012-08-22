dcc62381c3dd6689a2cd7ab6272ebfc01c1106fb
czluck <czluck@czluck-Inspiron-N4010.(none)>
2012-08-22 15:24:53 +0800

    [change] dist.php

--- dist.php
+++ dist.php
@@ -51,9 +51,10 @@ $distname = firstLetterToUpper($dist);
 <div id="search" style="float:right;padding top:80px;padding right:140px;font-size:30px;">
          <br>
          <br>
-        
+
          <form action="dist.php" method="post">
-         <p><input type="text" name="dist" class="input"> <input type="submit" class="sear_btn" value="搜索"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+         <p><input type="text" name="dist" class="input" style="width:30px;">
+         <input type="submit" class="sear_btn" value="搜索"> &nbsp;&nbsp;&nbsp;</p>
          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
          </form>
  </div>
