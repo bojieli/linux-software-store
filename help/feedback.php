a9205d368f3cab36bfd39d100f5eb36b4ec5e8eb
czluck <czlcuk123@gmail.com>
2012-08-25 13:48:31 +0800

    modified:   feedback.php

--- help/feedback.php
+++ help/feedback.php
@@ -2,38 +2,65 @@
 	<body>
 		<table border=0 style="height: 100%;width: 100%">
 			<tr>
-				<td align="center" style="height: 100%; width: 100%;font-size:15px;height: auto;">
+				<td align="center" style="height: 100%; width: 100%">
 					<table border=0 style="height: 100%">
 						<tr>
-						     <?php include"punlic/header.html"?>
-   						</tr>
-					   <tr>
-	    					<td class="main" colspan="3" style="height: auto;font-size:15px;"valign="top">
+							<td valign="middle" style="height: 100%">
+			                 <?php include"public/header.html"?>
+				               <div class="rounded_box lightgrey_borderless" style="margin-bottom: 5px;">
+				                  <div class="xboxcontent" style="vertical-align: middle; padding: 5px;">
+				                    <table border="0" cellpadding="0" cellspacing="0" style="width: 850px; height: 570px; table-layout: fixed;" dir="ltr" class="maintable">
+					                   <tr>
+					                     <td class="main" colspan="3" style="height: auto; padding-top: 5px;" valign="top">
+										    <?php include"public/update_time.html"?>
+							                <table>
+								                <tr>
+								                <td style="width: auto; padding: 5px 10px 0 5px;">
+									                    <p>感谢您参与</p>
+										                <br>
+										                <p>我们将在2～3个工作日为您解答</p>
+										                 <br>
+										                 <p>我们仅仅回复中文邮件，希望您能谅解。</p>
+										                 <br style="clear: both;">
+										                 <br>
+										                <div class="rounded_box white" style="margin-bottom: 10px; width: 200px;">
+											            <b class="xtop">
+											            <b class="xb1"></b>
+											            <b class="xb2"></b>
+											            <b class="xb3"></b>
+											            <b class="xb4"></b>
 
-							<br>
-							<br>
-							<b >感谢你的参与！</b>
-							<br>
-								<b>我们将在2～3个工作日对您的反馈进行回复</b>
-							<br>
-								<b>我们仅仅发送中文的回复</b><br>
-							<br>
-                                <form action="../index.html" method="POST">
-                                    <input type="submit" value="返回首页 >>" style="float left; font-size: 17px;color: green;">
-                                </form>
+											               <div class="xboxcontent" style="padding: 0; text-align: center;">
+												            <a href="../index.html" style="padding: 5px; font-size: 20px; font-weight: normal; border: 0; display: block;">
+												                <b>返回首页 &gt;&gt;</b>
+												            </a>
+											               </div>
+											                 <b class="xbottom">
+											                 <b class="xb4"></b>
+											                 <b class="xb3"></b>
+											                 <b class="xb2"></b>
+											                 <b class="xb1"></b>
 
-					        </td>
-						</tr>
+										                </div>
+										                <br>
 
-							</table>
-				</td>
+									                </td>
 
-						</tr>
-						</table>
+								                </tr>
+							                </table>
+						                 </td>
+					                   </tr>
+							        </table>
+					              </div>
 
-				</td>
-
-				</tr>
+					           </div>
+				            </td>
+				        </tr>
+				    </table>
+		        </td>
+		    </tr>
 		</table>
-</body>
+	 <script type="text/javascript" src="./js/google.js"></script>
+
+	</body>
 </html>
