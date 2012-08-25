d40a20b72e997d09ac88473115e2adc6bc8d1a0b
czluck <czlcuk123@gmail.com>
2012-08-25 13:51:59 +0800

    modified:   help/feedback.php

--- help/feedback.php
+++ help/feedback.php
@@ -5,7 +5,6 @@
 				<td align="center" style="height: 100%; width: 100%">
 					<table border=0 style="height: 100%">
 						<tr>
-<<<<<<< HEAD
 							<td valign="middle" style="height: 100%">
 			                 <?php include"public/header.html"?>
 				               <div class="rounded_box lightgrey_borderless" style="margin-bottom: 5px;">
@@ -30,7 +29,7 @@
 											            <b class="xb2"></b>
 											            <b class="xb3"></b>
 											            <b class="xb4"></b>
-
+                                                        </b>
 											               <div class="xboxcontent" style="padding: 0; text-align: center;">
 												            <a href="../index.html" style="padding: 5px; font-size: 20px; font-weight: normal; border: 0; display: block;">
 												                <b>返回首页 &gt;&gt;</b>
@@ -41,8 +40,8 @@
 											                 <b class="xb3"></b>
 											                 <b class="xb2"></b>
 											                 <b class="xb1"></b>
-=======
-						     <?php include "public/header.html"?>
+                                                             </b>
+
    						</tr>
 					   <tr>
 	    					<td class="main" colspan="3" style="height: auto;font-size:15px;"valign="top">
@@ -58,7 +57,7 @@
                                 <form action="../index.html" method="POST">
                                     <input type="submit" value="返回首页 >>" style="float left; font-size: 17px;color: green;">
                                 </form>
->>>>>>> 5c6705bc58edc206a486c6ebc734fed14d86c139
+
 
 										                </div>
 										                <br>
