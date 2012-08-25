5c6705bc58edc206a486c6ebc734fed14d86c139
Bojie Li <bojieli@gmail.com>
2012-08-25 12:28:09 +0800

    [fix] include paths in help/

--- help/feedback.php
+++ help/feedback.php
@@ -1,18 +1,18 @@
-<?php include"public/head.html"?>
+<?php include "public/head.html"?>
 	<body>
 		<table border=0 style="height: 100%;width: 100%">
 			<tr>
 				<td align="center" style="height: 100%; width: 100%;font-size:15px;height: auto;">
 					<table border=0 style="height: 100%">
 						<tr>
-						     <?php include"punlic/header.html"?>
+						     <?php include "public/header.html"?>
    						</tr>
 					   <tr>
 	    					<td class="main" colspan="3" style="height: auto;font-size:15px;"valign="top">
 
 							<br>
 							<br>
-							<b >感谢你的参与！</b>
+							<b>感谢你的参与！</b>
 							<br>
 								<b>我们将在2～3个工作日对您的反馈进行回复</b>
 							<br>
