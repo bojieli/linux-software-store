274a96a3664de200c814d2e866e214e31ee239a6
czluck <czlcuk123@gmail.com>
2012-08-27 10:14:47 +0800

    modified:   help/result.php

--- help/result.php
+++ help/result.php
@@ -68,8 +68,8 @@ include "public/head.html"?>
 	                                                                if (!$first) {
                                                                 ?>
                                                                 <dt><a href="../dist.php?dist=<?=$dist?>" title="点此去找<?=$dist?>的软件"><?=$dist?></a></dt>
-                                                                <dd>匹配程度：<?=$score*10?>%</dd>
-                                                                <div class="dist_png"><a href="../dist.php?dist=<?=$dist?>"><img src="../static/img/login/<?=$dist?>.png" title="点此去找<?=$dist?>的软件"></a></div>
+                                                                <dd>匹配程度：<?=$score*10?>%
+                                                                <div class="dist_png"><a href="../dist.php?dist=<?=$dist?>"><img src="../static/img/login/<?=$dist?>.png" title="点此去找<?=$dist?>的软件"></a></div></dd>
                                                                 <?php
 	                                                               }
 	                                                         $first = false;
