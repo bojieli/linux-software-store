ff710721cc7cfc751c23ebf7b42a4e4ea9300bf1
czluck <czlcuk123@gmail.com>
2012-08-31 13:25:04 +0800

    modified:   intro_new.php 	modified:   static/css/pack_intro.css

--- intro_new.php
+++ intro_new.php
@@ -29,12 +29,112 @@ include "static/public/head.html"
         <div id="header"></div>
         <!--this area is designed for show software information-->
         <div id="comm">
-        <div id="content-3"></div>
-        <div id="content-4">
-            <!-- UY BEGIN -->
-            <div id="uyan_frame"></div>
-            <script type="text/javascript" id="UYScript" src="http://v1.uyan.cc/js/iframe.js?UYUserId=1674756" async=""></script>
-            <!-- UY END -->
+            <div id="content-3">
+                <div id="intro">
+                    <table id="brf">
+                        <tr >
+                           <td class="sw_img">
+                              <img src="<?=$info['icon']?>" alt="此处显示<?=$package?>软件的图片">
+                           </td>
+                           <td class="sw_name">
+                              <?=$package?>
+                           </td>
+                        </tr>
+                    </table>
+                    <table class="detail">
+                        <tr class="sw_info">
+                           <td >
+                              <p>
+                                 软件大小：<?=print_filesize($info['filesize'])?>
+                              </p>
+                           </td>
+                           <td >
+                              <p>
+                                 应用平台：<?=$dist?> x86_64 , i386
+                              </p>
+                           </td>
+                        </tr>
+                        <tr class="sw_info">
+                           <td>
+                              <p>
+                                 更新时间：xx年xx月
+                              </p>
+                           </td>
+                           <td >
+                              <p>
+                                 软件授权：GPL
+                              </p>
+                           </td>
+                        </tr>
+                        <tr class="sw_info">
+                           <td>
+                              <p>
+                                 软件语言：UTF8,中文
+                              </p>
+                           </td>
+                           <td>
+                              <p>
+                                 软件分类：生活 网络 浏览器
+                              </p>
+                           </td>
+                        </tr>
+                           <tr class="sw_info">
+                              <td>
+                                 <pre>下载软件: <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">x86_64</a>       <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">i386</a>
+                                 </pre>
+                              </td>
+                              <td >
+                                 <pre>下载源码: <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">x86_64</a>  <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">i386</a>
+                                 </pre>
+                              </td>
+                        </tr>
+                        <tr class="sw_info">
+                           <td>
+                              <pre>软件安装:   <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">x86_64</a>  <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">i386</a>
+                              </pre>
+                           </td>
+                           <td>
+                              <pre>源码编译:   <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">x86_64</a>  <a href="http://mirrors.ustc.edu.cn/<?=$dist?>/">i386</a>
+                              </pre>
+                           </td>
+                        </tr>
+                        <tr class="sw_info">
+                           <td >
+                              <p>
+                                 推荐指数：
+                                 <img src="static/img/star.png" alt="star" width="20px" height="20px">
+                                 <img src="static/img/star.png" alt="star" width="20px" height="20px">
+                                 <img src="static/img/star.png" alt="star" width="20px" height="20px">
+                                 <img src="static/img/star2.png" alt="star2" width="20px" height="20px">
+                                 <img src="static/img/star2.png" alt="star2" width="20px" height="20px">
+                                 &nbsp;&nbsp;&nbsp;&nbsp;
+                              </p>
+                           </td>
+                           <td >
+                              <p>
+                                 软件评级：
+                                  <img src="static/img/star.png" alt="star" width="20px" height="20px">
+                                  <img src="static/img/star.png" alt="star" width="20px" height="20px">
+                                  <img src="static/img/star.png" alt="star" width="20px" height="20px">
+                                  <img src="static/img/star2.png" alt="star2" width="20px" height="20px">
+                                  <img src="static/img/star2.png" alt="star2" width="20px" height="20px">
+                                  &nbsp;&nbsp;&nbsp;&nbsp;
+                              </p>
+                           </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="sw_intro">
+                   <h1>软件简介</h1>
+                      &nbsp;&nbsp;<p style="text-align: left">/*<?=$info['description']?>*/<?=$package?>是一个好软件啊</p>
+                   </div>
+             </div>
+       
+            <div id="content-4">
+                 <!-- UY BEGIN -->
+                 <div id="uyan_frame"></div>
+                 <script type="text/javascript" id="UYScript" src="http://v1.uyan.cc/js/iframe.js?UYUserId=1674756" async=""></script>
+                 <!-- UY END -->
             </div>
         </div>
         </div>
