3c4cb492e2e7630d721bcd570d1f93348b2e246b
czluck <czlcuk123@gmail.com>
2012-09-01 19:24:10 +0800

    modified:   search_result.php

--- search_result.php
+++ search_result.php
@@ -63,9 +63,7 @@ include "static/public/head.html"
     <script type="text/javascript" src="http://v1.ujian.cc/code/ujian.js?type=slide&num=3&pos=left&btn=4&uid=1674756"></script>
     <!-- UJian Button END -->
     <!-- UY BEGIN -->
-    <div id="uyan_frame"></div>
-    <script type="text/javascript" id="UYScript" src="http://v1.uyan.cc/js/iframe.js?UYUserId=1674756" async=""></script>
-    <!-- UY END -->
+
 </div>
 </body>
 </html>
