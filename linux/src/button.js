f2e60dce3c370e1ae00b49df0c09e8bd3999ee5e
czluck <czlcuk123@gmail.com>
2012-09-01 10:43:11 +0800

    modified:   index.html

--- linux/src/button.js
+++
@@ -1,9 +0,0 @@
-function bitton_click ()
-{
-	document.getElementsByName("button").onclick=function change()
-	{
-	   document.getElementByName("button").style=document.write("background-image:url(./img/button-1.png);width=100px ;height=30px;border:0px; ");
-	   $(this).style=document.write("background-image:url(./img/button-2.png);width=100px ;height=30px;border:0px; ");	
-	   document.close();
-	}
-}
