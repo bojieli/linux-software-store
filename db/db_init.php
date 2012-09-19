c1f95542ab0c55208e194f38018dfea9c51aa37f
Bojie Li <bojieli@gmail.com>
2012-09-19 18:32:55 +0800

    db_init path

--- db/db_init.php
+++ db/db_init.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'db_config.php';
+include_once 'db/db_config.php';
 if (!mysql_pconnect($db['host'], $db['user'], $db['pass']))
 	die('Cannot connect to database!');
 if (!mysql_select_db($db['name']))
